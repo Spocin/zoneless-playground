a8fa19b072747d11b1e0cf03a72ad68741427fac
Michał Spociński <michal.spocinski.01@gmai.com>
2024-10-02 15:08:03 +0200

    Installed Angular Material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,11 +6,13 @@ import {
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideExperimentalZonelessChangeDetection(),
         provideExperimentalCheckNoChangesForDebug({ interval: 2000 }),
         provideRouter(routes),
+        provideAnimationsAsync(),
     ]
 };
