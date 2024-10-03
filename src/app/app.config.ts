28479f360f66339f85fb4fc06c85e69da684d612
Michał Spociński <michal.spocinski.01@gmai.com>
2024-10-04 01:53:06 +0200

    ng add @angular/ssr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,6 +7,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,5 +15,6 @@ export const appConfig: ApplicationConfig = {
 	provideExperimentalCheckNoChangesForDebug({interval: 2000}),
 	provideRouter(routes),
 	provideAnimationsAsync(),
+	provideClientHydration(),
   ]
 };
