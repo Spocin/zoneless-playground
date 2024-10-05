426fe0c12675b6f6bb1037b5865cf9d849b6a48f
Michał Spociński <michal.spocinski.01@gmai.com>
2024-10-05 23:52:30 +0200

    Added compression. Removed nginx

--- server.ts
+++ server.ts
@@ -4,6 +4,7 @@ import express from 'express';
 import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
 import bootstrap from './src/main.server';
+import compression from "compression";
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
@@ -16,29 +17,33 @@ export function app(): express.Express {
 
   server.set('view engine', 'html');
   server.set('views', browserDistFolder);
+  server.use(compression({
+	level: 6,
+	threshold: 0,
+  }));
 
   // Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
   server.get('**', express.static(browserDistFolder, {
-    maxAge: '1y',
-    index: 'index.html',
+	maxAge: '1y',
+	index: 'index.html',
   }));
 
   // All regular routes use the Angular engine
   server.get('**', (req, res, next) => {
-    const { protocol, originalUrl, baseUrl, headers } = req;
+	const { protocol, originalUrl, baseUrl, headers } = req;
 
-    commonEngine
-      .render({
-        bootstrap,
-        documentFilePath: indexHtml,
-        url: `${protocol}://${headers.host}${originalUrl}`,
-        publicPath: browserDistFolder,
-        providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
-      })
-      .then((html) => res.send(html))
-      .catch((err) => next(err));
+	commonEngine
+		.render({
+		  bootstrap,
+		  documentFilePath: indexHtml,
+		  url: `${protocol}://${headers.host}${originalUrl}`,
+		  publicPath: browserDistFolder,
+		  providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
+		})
+		.then((html) => res.send(html))
+		.catch((err) => next(err));
   });
 
   return server;
@@ -50,7 +55,7 @@ function run(): void {
   // Start up the Node server
   const server = app();
   server.listen(port, () => {
-    console.log(`Node Express server listening on http://localhost:${port}`);
+	console.log(`Node Express server listening on http://localhost:${port}`);
   });
 }
 
